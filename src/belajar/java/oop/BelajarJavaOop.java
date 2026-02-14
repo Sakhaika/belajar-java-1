b008f6079feb38bce788cf695597f1c5a6c268fd
Sakhaika <sakha00119@gmail.com>
2026-02-14 09:02:59 +0700

    belajar

---
+++ src/belajar/java/oop/BelajarJavaOop.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package belajar.java.oop;
+
+/**
+ *
+ * @author Sakhaika
+ */
+public class BelajarJavaOop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 17;
+        int angka = 5;
+
+        if (umur > 18){
+            System.out.println("Boleh masuk");
+        }
+        
+        angka = angka + 10;
+    }
+    
+}
